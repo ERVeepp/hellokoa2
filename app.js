ecd22e0b93c78e6d07bb9d940c69823eeb612c52
ERVee <736354882@qq.com>
2017-09-14 14:28:03 +0800

    项目创建
    20170914

---
+++ app.js
@@ -0,0 +1,55 @@
+const Koa = require('koa')
+const app = new Koa()
+const views = require('koa-views')
+const json = require('koa-json')
+const onerror = require('koa-onerror')
+const bodyparser = require('koa-bodyparser')
+const logger = require('koa-logger')
+const index = require('./routes/index')
+const users = require('./routes/users')
+const api = require('./routes/api')
+const router = require('koa-router')()
+const response_formatter = require('./middlewares/response_formatter');
+// error handler
+onerror(app)
+
+// middlewares
+app.use(bodyparser({
+  enableTypes:['json', 'form', 'text']
+}))
+app.use(json())
+app.use(logger())
+app.use(require('koa-static')(__dirname + '/public'))
+
+app.use(views(__dirname + '/views', {
+  extension: 'pug'
+}))
+
+// logger
+app.use(async (ctx, next) => {
+  const start = new Date()
+  await next()
+  const ms = new Date() - start
+  console.log(`${ctx.method} ${ctx.url} - ${ms}ms`)
+})
+
+router.use('/', index.routes(), index.allowedMethods());
+router.use('/users', users.routes(), users.allowedMethods());
+router.use('/api', api.routes(), api.allowedMethods());
+
+//添加格式化处理响应结果的中间件，在添加路由之前调用
+//仅对/api开头的url进行格式化处理
+app.use(response_formatter('^/api'));
+app.use(router.routes(), router.allowedMethods());
+
+// routes
+app.use(index.routes(), index.allowedMethods())
+app.use(users.routes(), users.allowedMethods())
+app.use(api.routes(), api.allowedMethods())
+
+// error-handling
+app.on('error', (err, ctx) => {
+  console.error('server error', err, ctx)
+})
+
+module.exports = app
---
+++ app/controllers/user_controller.js
@@ -0,0 +1,19 @@
+const ApiError = require('../error/ApiError');
+const ApiErrorNames = require('../error/ApiErrorNames');
+
+//获取用户
+exports.getUser = async (ctx, next) => {
+  //如果id != 1抛出API 异常
+  if(ctx.query.id != 1){
+      throw new ApiError(ApiErrorNames.USER_NOT_EXIST);
+  }
+  ctx.body = {
+      username: '王伟',
+      age: 26
+  }
+}
+
+//用户注册
+exports.registerUser = async (ctx, next) => {
+    console.log('registerUser', ctx.request.body);
+}
